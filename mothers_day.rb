1b8d4d79810c84c18d47ce3694074e7a2d3dcd93
Julia Cuervo <juliahcuervo@gmail.com>
2018-07-10 18:07:00 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,9 +1,7 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
-def mothers_day(name = Mom)
+def mothers_day(name = "Mom")
   puts "Happy Mother's day, #{name}!"
 end
-
 mothers_day
 mothers_day("Beyonce")
 
